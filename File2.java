2342816e6cdcd0588577b51d104c0b2bb268be9e
g2smtech <112677557+g2smtech@users.noreply.github.com>
2022-09-14 08:51:59 +0530

    Update File2.java

--- File2.java
+++ File2.java
@@ -2,11 +2,16 @@ Class File2 {
   public static void main(String args[]){
     System.out.println("file2 initial commit");
     int a, b=4, c=5;
+    //want to validate comments section
+    /**
+    Multi comments section as well to be valdiated
+    */
     System.out.println(b+c);
     System.out.println(b-c);
     System.out.println(a+b);
     System.out.println("validate merge conflicts");
     System.out.println("auto fix merge conflicts");
+    //Validating merge conflicts scenarios in same branch
     System.out.println("if not manually fix merge conflicts");
     
   }
