179b989e80ed7f2cfe29939a378ee3c8b150d300
g2smtech <112677557+g2smtech@users.noreply.github.com>
2022-09-14 08:54:34 +0530

    Update File1.java

--- File1.java
+++ File1.java
@@ -1,16 +1,18 @@
 class File1 {
 	public static void main(String args[])
 	{
+		//single line comments
 		System.out.println("file1 for merge changes");
 		System.out.println("add merge conflicts");
 		int a,b;
-		int c=a+b;
-		int c=a-b;
 		System.out.println("fix merge conflicts");
 		System.out.println("adding file1");
 		System.out.println(c);
 		int sub=subtract(3,5);
 		int sqrt = sqrt(4);
+		/**
+		Multi line comments
+		*/
 		System.out.println("merge conflicts");
 		System.out.println("print c value");
 
