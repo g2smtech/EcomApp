0a64619a3615ec20dfd7724be6812b81aceb656a
g2smtech <112677557+g2smtech@users.noreply.github.com>
2022-09-14 08:40:59 +0530

    Update File2.java

--- File2.java
+++ File2.java
@@ -3,5 +3,8 @@ Class File2 {
     System.out.println("file2 initial commit");
     int a, b=4, c=5;
     System.out.println(b+c);
+    System.out.println("validate merge conflicts");
+    System.out.println("auto fix merge conflicts");
+    System.out.println("if not manually fix merge conflicts");
   }
 }
