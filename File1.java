3df96b58467889e08053effc4f365b5fb175eb8d
gopiteekenam <gopikrishna.teekenam@gmail.com>
2022-09-14 08:58:19 +0530

    File1.java

--- File1.java
+++ File1.java
@@ -5,17 +5,19 @@ class File1 {
 		System.out.println("file1 for merge changes");
 		System.out.println("add merge conflicts");
 		int a,b;
+		//comment the lines to validate merge conflicts
 		System.out.println("fix merge conflicts");
 		System.out.println("adding file1");
 		System.out.println(c);
 		int sub=subtract(3,5);
+		//after printing system.out.println
 		int sqrt = sqrt(4);
 		/**
-		Multi line comments
+		Multi line comments to vaildate merge conflicts
 		*/
 		System.out.println("merge conflicts");
-		System.out.println("print c value");
-
+		System.out.println("print conflict value");
+		System.out.println("Is file really conflicted");
 	}
 	int sqrt(int a)
 	{
