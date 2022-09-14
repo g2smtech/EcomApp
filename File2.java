0788891bd35071ff6d29780784960dd9543e3423
chagantila2 <81638776+chagantila2@users.noreply.github.com>
2022-09-13 22:20:34 -0500

    Update File2.java

--- File2.java
+++ File2.java
@@ -3,8 +3,11 @@ Class File2 {
     System.out.println("file2 initial commit");
     int a, b=4, c=5;
     System.out.println(b+c);
+    System.out.println(b-c);
+    System.out.println(a+b);
     System.out.println("validate merge conflicts");
     System.out.println("auto fix merge conflicts");
     System.out.println("if not manually fix merge conflicts");
+    
   }
 }
