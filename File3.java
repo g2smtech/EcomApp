7992dec9d58961eee7791ad2f59fa66e248490cd
gopiteekenam <gopikrishna.teekenam@gmail.com>
2022-09-28 09:28:38 +0530

    file3

---
+++ File3.java
@@ -0,0 +1,26 @@
+Class File2 {
+  public static void main(String args[]){
+    System.out.println("file2 initial commit");
+    int a, b=4, c=5;
+    //want to validate comments section
+    /**
+    Multi comments section as well to be valdiated
+	comment2
+	comment3
+    */
+    System.out.println(b+c);
+    System.out.println(b-c);
+    System.out.println(a+b);
+    System.out.println("validate merge conflicts");
+    System.out.println("auto fix merge conflicts");
+    //Validating merge conflicts scenarios in same branch
+    System.out.println("if not manually fix merge conflicts");
+    a= add(b,c)
+	System.out.println("these are my other changes in my file2");
+	System.out.println("To validate stash we are running this file");
+    
+  }
+  int add(int b, intc )
+  {
+    System.out.println(b+c);
+}
